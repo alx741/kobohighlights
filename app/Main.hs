459a437d9f71d0b30a15c8ffdcd39606bed37f28
Daniel Campoverde <daniel.campoverde@ucuenca.edu.ec>
2021-09-07 12:46:55 -0500

    Upload user's DB file

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import           Control.Monad.IO.Class (liftIO)
+import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Data.IORef
 import           Data.Text
 import qualified Data.Text.IO           as TIO (readFile)
@@ -23,3 +23,24 @@ main = do
 app :: Text -> SpockM () MySession MyAppState ()
 app indexHtml = do
     get root $ html indexHtml
+    post "generate" generateHandler
+
+
+
+
+data TargetFormat
+    = EBOOK
+    | HTML
+    | PDF
+    deriving (Show, Read, Eq)
+
+
+generateHandler ::  MonadIO m => ActionCtxT ctx m a
+generateHandler = do
+    params  <- paramsPost
+    liftIO $ print params
+
+    fs <- files
+    liftIO $ print fs
+
+    file "placeholder File" "./kobohighlights.cabal"
