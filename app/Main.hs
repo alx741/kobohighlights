4a8fbd4422f74e9a7f7c30f56ed6bfb38cd46a3c
Daniel Campoverde <daniel.campoverde@ucuenca.edu.ec>
2021-10-26 12:35:11 -0500

    Generate final file

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,18 @@
 
 module Main where
 
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import qualified Data.ByteString        as BS (readFile)
-import qualified Data.HashMap.Strict    as HM
+import           Control.Monad.IO.Class    (MonadIO, liftIO)
+import qualified Data.ByteString           as BS (readFile)
+import qualified Data.HashMap.Strict       as HM
 import           Data.IORef
-import           Data.Maybe             (fromJust)
+import           Data.Maybe                (fromJust)
 import           Data.Text
-import qualified Data.Text.IO           as TIO (readFile)
-import           System.Directory       (copyFile, doesFileExist)
+import qualified Data.Text.IO              as TIO (readFile)
+import           Network.HTTP.Types.Status
+import           System.Directory          (copyFile, doesFileExist)
 import           System.Exit
-import           System.FilePath        (replaceExtensions)
-import           System.Process         (readProcessWithExitCode)
+import           System.FilePath           (replaceExtensions)
+import           System.Process            (readProcessWithExitCode)
 import           Web.Spock
 import           Web.Spock.Config
 
@@ -22,6 +23,10 @@ data MyAppState = DummyAppState (IORef Int)
 
 kobonotesBin :: FilePath
 kobonotesBin = "/opt/kobonotes"
+
+
+tmpDir :: FilePath
+tmpDir = "/opt/koboNotesTmp"
 
 
 main :: IO ()
@@ -36,8 +41,6 @@ app :: Text -> SpockM () MySession MyAppState ()
 app indexHtml = do
     get root $ html indexHtml
     post "generate" generateHandler
-
-
 
 
 data TargetFormat
@@ -58,13 +61,39 @@ generateHandler = do
     -- dat2 <- liftIO $ BS.readFile (uf_tempLocation $ fromJust dbFile)
     -- liftIO $ print dat2
 
-    liftIO $ copyFile (uf_tempLocation $ fromJust dbFile) "/home/alx/testfile"
+    liftIO $ copyFile (uf_tempLocation $ fromJust dbFile) $ tmpDir <> "/testfile"
+
+    case (lookup "targetFormat" params) of
+        Nothing      -> setStatus badRequest400
+        Just "EBOOK" -> (liftIO $ generateFinal EBOOK $ tmpDir <> "/testfile") >>= (\x -> liftIO $ print x)
+        Just "HTML"  -> (liftIO $ generateFinal HTML  $ tmpDir <> "/testfile") >>= (\x -> liftIO $ print x)
+        Just "PDF"   -> (liftIO $ generateFinal PDF   $ tmpDir <> "/testfile") >>= (\x -> liftIO $ print x)
+        Just _       -> setStatus badRequest400
 
     file "placeholder File" "./kobohighlights.cabal"
 
 
-generateFinal :: TargetFormat -> FilePath -> IO (Either Text FilePath)
-generateFinal dbFile targetFormat = undefined
+generate :: TargetFormat -> FilePath -> IO
+generate tf inputFile = undefined
+
+generateFinal :: TargetFormat -> FilePath -> IO (Either String FilePath)
+generateFinal EBOOK inputFile = performConvertion ebookParams inputFile $ replaceExtensions inputFile "epub"
+generateFinal HTML  inputFile = performConvertion htmlParams  inputFile $ replaceExtensions inputFile "html"
+generateFinal PDF   inputFile = performConvertion pdfParams   inputFile $ replaceExtensions inputFile "pdf"
+
+performConvertion params inputFile outputFile = do
+    (exitCode, _, err) <- readProcessWithExitCode "pandoc"
+        [ inputFile
+        , "-o " <> outputFile
+        , params
+        ] ""
+    case exitCode of
+        ExitFailure _ -> pure $ Left err
+        ExitSuccess   -> pure $ Right outputFile
+
+ebookParams = "--toc --toc-depth=1 --standalone --mathjax"
+htmlParams  = "--toc --toc-depth=1 --template ./template/template.html --standalone --mathjax"
+pdfParams   = "--toc --toc-depth=1 -H ./template/template.tex --standalone --mathjax -V geometry:margin=2cm"
 
 
 generateMarkdown :: FilePath -> IO (Either String FilePath)
